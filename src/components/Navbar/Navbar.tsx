91c05dbc086af84643623e550860f1cf372f5f31
egoryupatov <yupatovbusiness@gmail.com>
2023-02-25 17:36:08 +0600

    adaptive design

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -5,15 +5,17 @@ import Link from "next/link";
 export default function Navbar() {
   return (
     <nav className="navbar">
-      <div className="navbar_left">
-        <Link href="/">
-          <Image src="/logo.svg" width={56} height={20} alt="blog logo" />
-        </Link>
-      </div>
-      <div className="navbar_middle"></div>
-      <div className="navbar_right">
-        <Image src="/bell.svg" width={28} height={28} alt="notifications" />
-        <Button text={"Войти"} style={"btn_login"} />
+      <div className="navbar_layout">
+        <div className="navbar_layout_left">
+          <Link href="/">
+            <Image src="/logo.svg" width={56} height={20} alt="blog logo" />
+          </Link>
+        </div>
+        <div className="navbar_layout_middle"></div>
+        <div className="navbar_layout_right">
+          <Image src="/bell.svg" width={28} height={28} alt="notifications" />
+          <Button text={"Войти"} style={"btn_login"} />
+        </div>
       </div>
     </nav>
   );
